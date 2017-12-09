2f8e735ffc1339b0fb2241ca4e83c79dce45f5ff
Ashish Gupta <ashisgupta@ebay.com>
2017-12-09 09:40:59 -0500

    :construction: fixed setter and getter

--- src/main/scala/com/raids/ds/core/graph/Edge.scala
+++ src/main/scala/com/raids/ds/core/graph/Edge.scala
@@ -5,15 +5,16 @@ object EdgeDirection extends Enumeration{
 }
 
 trait Unweighted {
-  private val _weight:Float = 1.0f
+  val _weight:Float = 1.0f
   def weight: Float = _weight
   override def toString: String = new String(Array[Byte](0xE2.toByte, 0x80.toByte, 0x95.toByte))
 }
 
-trait Weighted extends Unweighted{
+trait Weighted {
   private var _weight: Float = 0.0f
   def * (multiplier: Float): Unit = _weight *= multiplier
   def + (increment: Float): Unit = _weight += increment
+  def weight: Float = _weight
   def weight_= (value: Float): Unit = _weight = value
 
   override def toString: String = new String(Array[Byte](0xE2.toByte, 0x94.toByte, 0x81.toByte))
