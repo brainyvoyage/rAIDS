f781634138df515e6e9b2a78c66db6a074fb5997
Ashish Gupta <ashisgupta@ebay.com>
2017-12-09 09:41:41 -0500

    :construction: Test case for directed weighted edge

--- src/test/scala/com/raids/ds/core/graph/UnDirectedWeightedEdgeTest.scala
+++ src/test/scala/com/raids/ds/core/graph/UnDirectedWeightedEdgeTest.scala
@@ -1,5 +1,43 @@
 package com.raids.ds.core.graph
 
-class UnDirectedWeightedEdgeTest {
+import org.scalatest.FunSuite
+
+class UnDirectedWeightedEdgeTest extends FunSuite {
+  val node1 = new Node[String](value = "node1", source = true)
+  val node2 = new Node[String](value = "node2", source = false)
+  val edge1 = new UnDirectedWeightedEdge[String](n1 = node1, n2 = node2)
+
+
+  test("testWeight") {
+    val weight = edge1.weight
+    assert(weight == 0.0f)
+
+    edge1.weight = 2.0f
+    val result = edge1.weight
+    assert(result == 2.0f)
+  }
+
+  test("test$times") {
+    edge1.weight *= 3.0f
+    assert(edge1.weight == 6.0f)
+  }
+
+  test("test$plus") {
+    edge1.weight += 2.3f
+    assert(edge1.weight == 8.3f)
+  }
+
+  test("testToString") {
+    val result = edge1.toString
+    val expected = node1.toString +
+      new String(Array[Byte](0xE2.toByte, 0x94.toByte, 0x81.toByte)) +
+      node2.toString
+    assert(result == expected)
+  }
+
+  test("testWeight_$eq") {
+    edge1.weight = 2.345f
+    assert(edge1.weight == 2.345f)
+  }
 
 }
