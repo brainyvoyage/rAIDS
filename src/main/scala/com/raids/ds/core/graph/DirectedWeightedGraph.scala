6101a71e01e44e050230f18bc61058e2e916d6cf
ashisgupta <ashisgupta@ebay.com>
2017-12-13 12:12:06 -0500

    :construction: test case for vertex

---
+++ src/main/scala/com/raids/ds/core/graph/DirectedWeightedGraph.scala
@@ -0,0 +1,11 @@
+package com.raids.ds.core.graph
+
+import com.raids.ds.core.graph.edge.WeightedDirectedEdge
+
+import scala.collection.mutable
+
+class DirectedWeightedGraph[T] extends DirectedGraph[T]{
+  val adjList: mutable.Map[
+    Vertex[T], mutable.Set[WeightedDirectedEdge[T]]] = mutable.Map[Vertex[T], mutable.Set[WeightedDirectedEdge[T]]]()
+
+}
