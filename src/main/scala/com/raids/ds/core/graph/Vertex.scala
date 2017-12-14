42fd50586090120788cfcd9a0c85e7d9c2a2027f
ashisgupta <brainyvoyage@gmail.com>
2017-12-14 18:44:27 -0500

    :construction: Removed unnecessary type parmeterisation

--- src/main/scala/com/raids/ds/core/graph/Vertex.scala
+++ src/main/scala/com/raids/ds/core/graph/Vertex.scala
@@ -4,7 +4,7 @@ import com.raids.Data
 
 import scala.language.higherKinds
 
-class Vertex[A](val data:Data[A]) extends States {
+class Vertex[A](val data:Data[_]) extends States {
   private[this] var _state:Value = unDiscovered
   private[this] var _discoveryTime:Int = 0
   private[this] var _finishingTime:Int = -1
@@ -25,7 +25,7 @@ class Vertex[A](val data:Data[A]) extends States {
 
   def predecessor:Vertex[A] = _predecessor
   @throws(classOf[IllegalArgumentException])
-  def predecessor_=(that: Vertex[A]): Unit = {
+  def predecessor_=(that : Vertex[A]): Unit = {
     require(that.discoveryTime < this._predecessor.discoveryTime)
     _predecessor = that
   }
